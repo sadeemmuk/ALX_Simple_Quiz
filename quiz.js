c561d8dd025123985cb1c3a573b0632faf1ea297
sadeemmuk <sadeemmuk2012@gmail.com>
2024-07-14 07:39:21 +0300

    update js file

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer(){
     let correctAnswer = 4;
     let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
     
-    if(userAnswer == correctAnswer){
+    if(userAnswer === correctAnswer){
         document.getElementById('feedback').textContent = "That's correct! Well done.";
     }else{
         document.getElementById('feedback').textContent ="It is incorrect! Try again.";
